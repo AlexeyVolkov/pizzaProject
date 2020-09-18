adb8448a4215632c2c4948592375b7357e502b47
Алексей Волков <av@molinos.ru>
2020-09-18 03:59:30 +0300

    build heroku

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ $title }} | {{ __('PizzaProject') }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     @livewireStyles
 </head>
 <body>
 {{ $slot }}
 @livewireScripts
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
