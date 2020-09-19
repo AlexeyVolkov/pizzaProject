a76b97eb54493644a4bd26ae24346bc08b9de4e1
Алексей Волков <av@molinos.ru>
2020-09-19 13:11:16 +0300

    Replace currency name with icons

--- resources/views/livewire/currency-switch.blade.php
+++ resources/views/livewire/currency-switch.blade.php
@@ -1,10 +1,12 @@
 <div>
     <nav class="list-group list-group-horizontal currency-switch">
         @foreach($currencies as $currency)
-            <a href="#{{ $currency->name }}"
-               class="currency-switch__item list-group-item list-group-item-action {{ $currencyId == $currency->id ? 'list-group-item-dark active' : '' }}"
-            wire:click.prevent="currencyChange({{ $currency->id }})">
-                {{ $currency->name }}
+            <a
+                    wire:click.prevent="currencyChange({{ $currency->id }})"
+                    href="#{{ $currency->name }}"
+                    class="currency-switch__item list-group-item list-group-item-action {{ $currencyId == $currency->id ? 'list-group-item-dark active' : '' }}"
+                    title="{{ $currency->name }}">
+                <i class="{{ $currency->icon_class }}"></i>
             </a>
         @endforeach
     </nav>
